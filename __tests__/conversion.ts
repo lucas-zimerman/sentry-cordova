51d9d2b904a9795db18894adb7db15c92d5f8628
jennmueng <ldrwarlock@gmail.com>
2021-03-18 17:06:18 +0700

    fix(tests): Remove check to not require @sentry/browser
    We require @sentry/browser

--- __tests__/conversion.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-describe('JS conversion check', () => {
-  test('Do not require @sentry/browser', () => {
-    const content = fs.readFileSync('dist/js/sentry-cordova.bundle.js');
-    // expect(content.toString().match(/^[^\*]*@sentry\/browser/gm)).toBeFalsy();
-    expect(content.toString().match(/ require\(/)).toBeFalsy();
-  });
-});
